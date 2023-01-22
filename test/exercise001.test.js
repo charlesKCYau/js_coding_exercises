a56f97643bb7c58192520423a30164983912987e
yaunga@hotmail.com <yaunga@hotmail.com>
2023-01-22 00:45:03 +0000

    mac test

--- test/exercise001.test.js
+++ test/exercise001.test.js
@@ -35,6 +35,10 @@ describe("generateInitials", () => {
   test("returns the initials of a firstname and surname", () => {
     expect(generateInitials("Frederic", "Bonneville")).toBe("F.B");
   });
+
+  test("empty string", () => {
+    expect(generateInitials("", "")).toBe(".");
+  });
 });
 
 xdescribe("addVAT", () => {
