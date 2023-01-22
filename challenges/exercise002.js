8607094cf0abcaa8a86731891d8234f95770e354
yaunga@hotmail.com <yaunga@hotmail.com>
2023-01-22 17:51:46 +0000

    Ex 2 finished

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,31 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  if (isNaN(people)) throw new Error("people must be number");
+  return Math.ceil(people/40);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  if (! Array.isArray(arr)) throw new Error("array is required");
+  let shpCnt = 0;
+  for (let i=0; i<arr.length; i++) {
+     if (arr[i] === "sheep") shpCnt++;
+  }
+  return shpCnt;
+
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.address.postCode.substr(0,1) === "M" && person.address.city === "Manchester";
 }
